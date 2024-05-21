8d696d820afa6fea008a375aaf53748bc48b2b65
tolji <toljik1@mail.ru>
2024-05-22 01:06:36 +0400

    add utils

---
+++ src/utils.py
@@ -0,0 +1,45 @@
+def get_vacancies_by_salary(vacancies_data: list, salary_range: str):
+    """
+    Фильтрует вакансии в пределах заданного диапазона зарплат.
+
+    :param vacancies_data: Список объектов вакансий для фильтрации.
+    :param salary_range: Строка, указывающая минимальное и максимальное значения заработной платы.
+
+    :return: Отфильтрованный список объектов вакансий в пределах диапазона заработной платы.
+    """
+    min_salary, max_salary = map(int, salary_range.split('-'))
+    ranged_vacancies = []
+    for vacancy in vacancies_data:
+        if vacancy.salary_from is not None and vacancy.salary_to is not None:
+            if vacancy.salary_from >= min_salary and vacancy.salary_to <= max_salary:
+                ranged_vacancies.append(vacancy)
+    return ranged_vacancies
+
+
+def sort_vacancies(vacancies_data: list):
+    """
+    Сортирует список вакансий по начальной зарплате в порядке убывания.
+
+    :param vacancies_data: Список объектов вакансий для сортировки.
+
+    :return: Отсортированный список объектов вакансий.
+    """
+    return sorted(vacancies_data, key=lambda x: x.salary_from, reverse=True)
+
+
+def get_top_vacancies(vacancies_data: list, top_n):
+    """
+    Возвращает N лучших вакансий из отсортированного списка.
+
+    :param vacancies_data: Отсортированный список объектов вакансий.
+    :param top_n: Количество вакансий для возврата.
+
+    :return: Топ N вакансий.
+    """
+    return vacancies_data[:top_n]
+
+
+
+def filter_vacancies(vacancies, filter_words):
+
+    return [vac for vac in vacancies if vac.description and any(word in vac.description for word in filter_words)]
